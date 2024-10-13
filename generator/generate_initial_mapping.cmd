18ead7b4a7628cdd91ab9404c926d05ca54ec4f7
vaiorabbit <vaiorabbit>
2024-10-14 00:33:40 +0900

    Fix Windows script

--- generator/generate_initial_mapping.cmd
+++ generator/generate_initial_mapping.cmd
@@ -1,4 +1,3 @@
-#!/usr/local/bin/zsh
 set PYTHONPATH=.\clang
 python generate_initial_cindex_mapping.py > box2d_cindex_mapping.json
 python generate_initial_define_mapping.py > box2d_define_mapping.json
