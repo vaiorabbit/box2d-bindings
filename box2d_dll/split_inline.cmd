f4127b7107500741cd2c77e51ebe7d5cf640214b
vaiorabbit <vaiorabbit>
2024-12-31 02:43:02 +0900

    Added several missing methods (b2StoreBodyId, etc.), that should be in the DLLs

--- box2d_dll/split_inline.cmd
+++ box2d_dll/split_inline.cmd
@@ -1,6 +1,8 @@
 @echo off
 setlocal enabledelayedexpansion
 
+echo add_compile_definitions(box2d_EXPORTS) >> %~dp0\box2d\CMakeLists.txt
+
 ruby split_inline_collision.rb
 ruby split_inline_id.rb
 ruby split_inline_math_functions.rb
--- box2d_dll/split_inline.sh
+++ box2d_dll/split_inline.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+echo "add_compile_definitions(box2d_EXPORTS)" >> ./box2d/CMakeLists.txt
+
 ruby split_inline_collision.rb
 ruby split_inline_id.rb
 ruby split_inline_math_functions.rb
