4c1c7a5b03ced5fb153414428b26b572a75a2788
vaiorabbit <vaiorabbit>
2024-12-31 02:03:18 +0900

    Added several missing methods (b2StoreBodyId, etc.), that should be in the DLLs

--- box2d_dll/linux_build.sh
+++ box2d_dll/linux_build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir -p build
 cd build
-cmake -D CMAKE_C_FLAGS="" -D CMAKE_BUILD_TYPE=Release -D CMAKE_C_COMPILER=clang -D BOX2D_SAMPLES=OFF -D BOX2D_VALIDATE=OFF -D BOX2D_UNIT_TESTS=OFF -D BUILD_SHARED_LIBS=ON ../box2d
+cmake -D CMAKE_C_FLAGS="" -D CMAKE_BUILD_TYPE=Release -D CMAKE_C_COMPILER=clang -Dbox2d_EXPORTS=1 -D BOX2D_SAMPLES=OFF -D BOX2D_VALIDATE=OFF -D BOX2D_UNIT_TESTS=OFF -D BUILD_SHARED_LIBS=ON ../box2d
 cmake --build .
 export ARCH=`uname -m`
 cp src/libbox2d.so ../../lib/libbox2d.${ARCH}.so
