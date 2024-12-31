f4c183536fbf0a5b236ae701faf21db6a3f83f83
vaiorabbit <vaiorabbit>
2024-12-31 14:25:05 +0900

    Fixed build script

--- box2d_dll/linux_build.sh
+++ box2d_dll/linux_build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+bash ./split_inline.sh
+
 mkdir -p build
 cd build
 cmake -D CMAKE_C_FLAGS="" -D CMAKE_BUILD_TYPE=Release -D CMAKE_C_COMPILER=clang -Dbox2d_EXPORTS=1 -D BOX2D_SAMPLES=OFF -D BOX2D_VALIDATE=OFF -D BOX2D_UNIT_TESTS=OFF -D BUILD_SHARED_LIBS=ON ../box2d
