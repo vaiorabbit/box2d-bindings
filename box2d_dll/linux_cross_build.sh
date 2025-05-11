05e6ab16391481a500e504f12b6fd896b6c8e5ca
vaiorabbit <vaiorabbit>
2025-05-11 15:52:58 +0900

    Fix build script

--- box2d_dll/linux_cross_build.sh
+++ box2d_dll/linux_cross_build.sh
@@ -7,5 +7,5 @@ cd build
 cmake -D CMAKE_C_FLAGS=-isystem\ /usr/aarch64-linux-gnu/include -D CMAKE_BUILD_TYPE=Release -D CMAKE_C_COMPILER_TARGET=aarch64-linux-gnu -D CMAKE_SYSTEM_PROCESSOR=ARM -Dbox2d_EXPORTS=1 -D BOX2D_SAMPLES=OFF -D BOX2D_VALIDATE=OFF -D BOX2D_UNIT_TESTS=OFF -D BUILD_SHARED_LIBS=ON -D CMAKE_C_COMPILER=clang ../box2d
 cmake --build .
 export ARCH=aarch64
-cp src/libbox2d.so ../../lib/libbox2d.${ARCH}.so
+cp bin/libbox2d.so ../../lib/libbox2d.${ARCH}.so
 
--- box2d_dll/macos_build.sh
+++ box2d_dll/macos_build.sh
@@ -7,7 +7,7 @@ mkdir -p build_x86_64
 cd build_x86_64
 cmake -D CMAKE_C_FLAGS="" -D CMAKE_BUILD_TYPE=Release -D CMAKE_OSX_ARCHITECTURES="x86_64" -D CMAKE_C_COMPILER=clang -Dbox2d_EXPORTS=1 -D BOX2D_SAMPLES=OFF -D BOX2D_VALIDATE=OFF -D BOX2D_UNIT_TESTS=OFF -D BUILD_SHARED_LIBS=ON ../box2d
 cmake --build .
-cp src/libbox2d.dylib ../../lib/libbox2d.x86_64.dylib
+cp bin/libbox2d.dylib ../../lib/libbox2d.x86_64.dylib
 
 cd ..
 
@@ -15,4 +15,4 @@ mkdir -p build_arm64
 cd build_arm64
 cmake -D CMAKE_C_FLAGS="" -D CMAKE_BUILD_TYPE=Release -D CMAKE_OSX_ARCHITECTURES="arm64" -D CMAKE_C_COMPILER=clang -D BOX2D_SAMPLES=OFF -D BOX2D_VALIDATE=OFF -D BOX2D_UNIT_TESTS=OFF -D BUILD_SHARED_LIBS=ON ../box2d
 cmake --build .
-cp src/libbox2d.dylib ../../lib/libbox2d.arm64.dylib
+cp bin/libbox2d.dylib ../../lib/libbox2d.arm64.dylib
