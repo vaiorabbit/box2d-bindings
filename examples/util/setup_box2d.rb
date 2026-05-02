08d9375ac5f45e94dc7ab8afc5158adaf31f9d30
vaiorabbit <vaiorabbit>
2026-05-02 13:58:53 +0900

    Fixed typo

--- examples/util/setup_box2d.rb
+++ examples/util/setup_box2d.rb
@@ -26,7 +26,7 @@ if box2d_bindings_gem_available?
     raise RuntimeError, "setup_dll.rb : Unknown OS: #{RUBY_PLATFORM}"
   end
 else
-  # puts("Loaging from local path.")
+  # puts("Loading from local path.")
   require '../lib/box2d'
 
   case RUBY_PLATFORM
